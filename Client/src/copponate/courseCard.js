7043e87ea7c59a7d754b57d5fb94ebac005099ed
Pankaj <pankajjjn60@gmail.com>
2023-06-10 16:29:39 +0530

    first commit

--- Client/src/copponate/courseCard.js
+++ Client/src/copponate/courseCard.js
@@ -1,16 +1,16 @@
 import React from 'react'
-// import img from '../images/img1.jpg'
+import { Link } from 'react-router-dom'
 export default function CourseCard(props) {
     return (
-            <div className="col-md-4 col-10 my-3 ">
-                <div className="card" >
-                    <img src={props.img} className="card-img-top img-height " alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">{props.title}</h5>
-                        <p className="card-text">{props.discription}...</p>
-                        <a href="/" className="btn  btn-secondary">Start Watching</a>
-                    </div>
+        <div className="col-md-4 col-12 my-3 mx-auto">
+            <div className="card" >
+                <img src={props.img} className="card-img-top img-height " alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{props.title}</h5>
+                    <p className="card-text">{props.discription}...</p>
+                    <Link to="/Notes" className="btn  btn-secondary">Start Watching</Link>
                 </div>
             </div>
+        </div>
     )
 }
